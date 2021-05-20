a790bb028a88353a76ebf6e6bca5a41b86afa9fb
Dmytro Bereznii <dima.berezniy.13@gmail.com>
2021-05-20 22:22:00 +0300

    fix: extend cmc_rank

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -69,9 +69,9 @@ class Currency extends Model
                 DB::raw("CONCAT(cmc_rank,'. ',symbol,' ',name) as symbol")
             ])
             ->whereNotIn('cmc_id', Asset::getUserCurrencies())
-            ->where('cmc_rank', '<=', 100)
+            ->where('cmc_rank', '<=', 400)
             ->orderBy('cmc_rank')
-            ->limit(100)
+            ->limit(400)
             ->get()
             ->pluck('symbol', 'cmc_id')
             ->toArray();
